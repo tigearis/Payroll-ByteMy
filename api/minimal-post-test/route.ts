5f52951c0a8d3b12fe080b5bee64e49126ab92d9
NJHarris84 <nathan.harris@invenco.net>
2025-06-20 17:40:33 +1000

    Fix Folder

--- api/minimal-post-test/route.ts
+++
@@ -1,4 +0,0 @@
-export async function POST() {
-  console.log("MINIMAL POST - Handler executing");
-  return Response.json({ message: "minimal POST works" });
-}
--- api/test-get-public/route.ts
+++
@@ -1,4 +0,0 @@
-export async function GET() {
-  console.log("TEST GET - Handler executing");
-  return Response.json({ message: "GET works" });
-}
