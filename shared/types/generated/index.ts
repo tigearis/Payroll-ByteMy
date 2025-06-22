f942adef70cc8f3714f899c272196b70368b8094
NJHarris84 <nathan.harris@invenco.net>
2025-06-22 11:24:54 +1000

    fixes to authentication

--- shared/types/generated/index.ts
+++ shared/types/generated/index.ts
@@ -22,25 +22,9 @@
 
 // Central export aggregator for all GraphQL operations
 
-// Re-export fragment masking utilities
-export * from './fragment-masking';
-
 // Re-export gql utilities
 export * from './gql';
 
 // Re-export base types and generated operations
 export * from './graphql';
-
-// Auto-aggregate domain exports
-export * from '../../../domains/auth/graphql/generated';
-export * from '../../../domains/audit/graphql/generated';
-export * from '../../../domains/permissions/graphql/generated';
-export * from '../../../domains/users/graphql/generated';
-export * from '../../../domains/clients/graphql/generated';
-export * from '../../../domains/billing/graphql/generated';
-export * from '../../../domains/payrolls/graphql/generated';
-export * from '../../../domains/notes/graphql/generated';
-export * from '../../../domains/leave/graphql/generated';
-export * from '../../../domains/work-schedule/graphql/generated';
-export * from '../../../domains/external-systems/graphql/generated';
 // Shared operations exported directly above
