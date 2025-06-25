4e5dc74dc2b1743622badc076f1e337a424522c2
NJHarris84 <nathan.harris@invenco.net>
2025-06-25 23:28:00 +1000

    fix payyrolls table

--- domains/billing/graphql/generated/graphql.ts
+++ domains/billing/graphql/generated/graphql.ts
@@ -16,7 +16,7 @@
  * ✓ Permission boundary validation
  * ✓ Automatic domain isolation and exports
  * 
- * Generated: 2025-06-25T13:11:57.884Z
+ * Generated: 2025-06-25T13:21:46.492Z
  * Schema Version: Latest from Hasura
  * CodeGen Version: Unified v3.0
  */
