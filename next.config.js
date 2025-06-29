d4aaea83bee5c593d0d04f90e0847f501d3e7288
NJHarris84 <nathan.harris@invenco.net>
2025-06-29 12:00:01 +1000

    update next.config

--- next.config.js
+++ next.config.js
@@ -75,7 +75,7 @@ const nextConfig = {
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
