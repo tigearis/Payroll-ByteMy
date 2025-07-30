3e58f35af947de3c28b237f6ffb620c55b4259b8
NJHarris84 <nathan.harris@invenco.net>
2025-07-30 15:56:47 +1000

    security

--- shared/types/generated/graphql.ts
+++ shared/types/generated/graphql.ts
@@ -18,7 +18,7 @@
  * ✓ Client Preset v4.8+ for optimal type safety
  * ✓ Zero type conflicts with modern codegen
  * 
- * Generated: 2025-07-30T05:18:30.814Z
+ * Generated: 2025-07-30T05:55:58.367Z
  * Schema Version: Latest from Hasura
  * CodeGen Version: Client Preset v4.0
  */
